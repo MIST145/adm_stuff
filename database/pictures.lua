efe6357f1b5ae1a93bcc050b55003053ad80b17a
Mist <114344332+MIST145@users.noreply.github.com>
2025-12-25 04:12:31 +0000

    Update pictures.lua - Added 27 new entries

--- database/pictures.lua
+++ database/pictures.lua
@@ -1,3 +1,111 @@
 -- FiveManage Images
 return {
+    ["bsfugitive"] = {
+        label = "bsfugitive",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/bsfugitive.png"
+    },
+    ["bsmesa"] = {
+        label = "bsmesa",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/bsmesa.png"
+    },
+    ["bspony"] = {
+        label = "bspony",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/bspony.png"
+    },
+    ["bsseminolef"] = {
+        label = "bsseminolef",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/bsseminolef.png"
+    },
+    ["bssurge"] = {
+        label = "bssurge",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/bssurge.png"
+    },
+    ["chfbrick"] = {
+        label = "chfbrick",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/chfbrick.png"
+    },
+    ["chfchavos"] = {
+        label = "chfchavos",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/chfchavos.png"
+    },
+    ["chflokus"] = {
+        label = "chflokus",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/chflokus.png"
+    },
+    ["chfseminole"] = {
+        label = "chfseminole",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/chfseminole.png"
+    },
+    ["dilettante2"] = {
+        label = "dilettante2",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/dilettante2.png"
+    },
+    ["g6buffalo"] = {
+        label = "g6buffalo",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/g6buffalo.png"
+    },
+    ["g6caddy"] = {
+        label = "g6caddy",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/g6caddy.png"
+    },
+    ["g6perennial"] = {
+        label = "g6perennial",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/g6perennial.png"
+    },
+    ["g6rcv"] = {
+        label = "g6rcv",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/g6rcv.png"
+    },
+    ["g6speedo"] = {
+        label = "g6speedo",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/g6speedo.png"
+    },
+    ["g6stockade"] = {
+        label = "g6stockade",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/g6stockade.png"
+    },
+    ["llexecutioner"] = {
+        label = "llexecutioner",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/llexecutioner.png"
+    },
+    ["llmerit"] = {
+        label = "llmerit",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/llmerit.png"
+    },
+    ["llsecuricar"] = {
+        label = "llsecuricar",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/llsecuricar.png"
+    },
+    ["llsteed"] = {
+        label = "llsteed",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/llsteed.png"
+    },
+    ["mwgranger"] = {
+        label = "mwgranger",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/mwgranger.png"
+    },
+    ["mwgresley"] = {
+        label = "mwgresley",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/mwgresley.png"
+    },
+    ["mwpremier"] = {
+        label = "mwpremier",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/mwpremier.png"
+    },
+    ["mwstanier"] = {
+        label = "mwstanier",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/mwstanier.png"
+    },
+    ["secpigeon"] = {
+        label = "secpigeon",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/secpigeon.png"
+    },
+    ["stockade"] = {
+        label = "stockade",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/stockade.png"
+    },
+    ["stockade2"] = {
+        label = "stockade2",
+        image = "https://raw.githubusercontent.com/MIST145/adm_stuff/main/security_pack/stockade2.png"
+    }
 }
